c05d9904daff3ce5998b17d452f9d1b6b3ba1d0f
xavi <xabi1309@gmail.com>
2020-08-11 19:34:18 +0200

    Add test db connection to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,11 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 
-  # config.before(:suite) { DatabaseCleaner.clean_with(:truncation) }
-  # config.before(:each) { DatabaseCleaner.strategy = :transaction }
-  # config.before(:each, :js => true) { DatabaseCleaner.strategy = :truncation }
-  # config.before(:each) { DatabaseCleaner.start }
-  # config.after(:each) { DatabaseCleaner.clean }
+  config.before(:suite) { DatabaseCleaner.clean_with(:truncation) }
+  config.before(:each) { DatabaseCleaner.strategy = :transaction }
+  config.before(:each, :js => true) { DatabaseCleaner.strategy = :truncation }
+  config.before(:each) { DatabaseCleaner.start }
+  config.after(:each) { DatabaseCleaner.clean }
 
   FactoryBot.find_definitions
 
@@ -21,3 +21,5 @@ def db_configuration
   db_configuration_file = File.join(File.expand_path('..', __FILE__), '..', 'db', 'config.yml')
   YAML.load(File.read(db_configuration_file))
 end
+
+ActiveRecord::Base.establish_connection(db_configuration["test"])
